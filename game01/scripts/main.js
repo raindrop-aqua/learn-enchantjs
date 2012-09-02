e3d58249ee59c65c8b98d5020ce0a92e4010865c
Masahiro Atsumi <raindrop.aqua@gmail.com>
2012-09-03 01:55:11 +0900

    add game01

---
+++ game01/scripts/main.js
@@ -0,0 +1,18 @@
+
+enchant();
+
+
+window.onload = function() {
+    var game = new Game(320, 320);
+
+    game.fps = 15;
+    game.preload("images/chara1.png");
+    game.onload = function() {
+
+
+
+
+    };
+    game.start();
+};
+
