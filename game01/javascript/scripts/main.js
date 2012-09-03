5b07de1682a22166d874e5e82af5b7959e9366cc
Masahiro Atsumi <raindrop.aqua@gmail.com>
2012-09-03 22:24:26 +0900

    main.js implemented.

--- game01/javascript/scripts/main.js
+++ game01/javascript/scripts/main.js
@@ -8,10 +8,19 @@ window.onload = function() {
     game.fps = 15;
     game.preload("images/chara1.png");
     game.onload = function() {
-
-
-
-
+        bear = new Sprite(32, 32);
+        bear.image = game.assets["images/chara1.png"];
+        bear.x = 0;
+        bear.y = 0;
+        bear.frame = 5;
+        game.rootScene.addChild(bear);
+        bear.addEventListener("enterframe", function(){
+            this.x += 1;
+            this.frame = this.age % 2 + 6;
+        });
+        bear.addEventListener("touchstart", function(){
+            game.rootScene.removeChild(bear);
+        });
     };
     game.start();
 };
