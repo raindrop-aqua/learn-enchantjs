ac780bf168086f83387feb9e3c9e7770647365a6
Masahiro Atsumi <raindrop.aqua@gmail.com>
2012-09-11 01:12:52 +0900

    Change scope variable bear
    global -> private

--- game01/javascript/scripts/main.js
+++ game01/javascript/scripts/main.js
@@ -8,7 +8,7 @@ window.onload = function() {
     game.fps = 15;
     game.preload("images/chara1.png");
     game.onload = function() {
-        bear = new Sprite(32, 32);
+        var bear = new Sprite(32, 32);
         bear.image = game.assets["images/chara1.png"];
         bear.x = 0;
         bear.y = 0;
